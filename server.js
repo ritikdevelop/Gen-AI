c792fdf2eecd6b4e280769cbf3b079c368e14f32
ritikdevelop <hrithikrana216@gmail.com>
2026-03-10 12:39:06 +0530

    beautify the server file

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
-const app = require('./src/app');
+require("dotenv").config();
+const app = require("./src/app");
 
-const connectDB = require('./src/config/db');
+const connectDB = require("./src/config/db");
 
 connectDB();
 
 app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-})
-
+    console.log("Server is running on port 4000");
+});
