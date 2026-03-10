61649ab0a49c4a3558123cd750adda2c71e664c4
ritikdevelop <hrithikrana216@gmail.com>
2026-03-10 12:41:03 +0530

    feat: Initialize Express application with core middleware and an authentication route.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
-const express = require('express');
-
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-
 app.use(express.json());
+app.use(cookieParser());
 
-const authRouter = require('./routes/auth.route');
+const authRouter = require("./routes/auth.route");
 
-app.use('/api/auth', authRouter);
-
+app.use("/api/auth", authRouter);
 
 module.exports = app;
