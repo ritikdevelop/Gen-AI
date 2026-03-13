98cb56e4981ef4e29a43284acf11b6305d6e8b69
ritikdevelop <hrithikrana216@gmail.com>
2026-03-13 16:00:20 +0530

    feat: initialize application with authentication and interview report generation and viewing features.

---
+++ frontend/src/features/auth/components/protected.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+
+const Protected = ({ children }) => {
+  const { loading, user } = useAuth();
+
+  if (loading) return <div>Loading...</div>;
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+
+export default Protected;
